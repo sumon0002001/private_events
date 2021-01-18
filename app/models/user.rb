16fae18cf9d5d2b9fa7133995a221d7702acf55a
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-01-19 03:01:50 +0600

    added associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
-end
+    has_many :attendees
+    has_many :attended_events, through: :attendees, foreign_key: 'event_id', source: :event
+  
+    has_many :authored_events, foreign_key: 'author_id', class_name: 'Event'
+  
+    validates :name, presence: true, uniqueness: true, length: { minimum: 4, message: 'Name is too short...' }
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'users#index'
   resources :user, only: [:show, :new, :create]
+  resources :event, only: [:index, :new, :create, :show, :destroy]
   
   
  
